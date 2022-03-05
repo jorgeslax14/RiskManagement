f76f68ab4dc19f2e997c5ff3ad5965ce86ceedd0
jorgeslax14 <ingeniero.jorgevelasquez@gmail.com>
2022-03-05 16:52:16 +0000

    Primero commit CAP-SAP

---
+++ app/ricks/webapp/Component.js
@@ -0,0 +1,9 @@
+sap.ui.define(["sap/fe/core/AppComponent"], function(AppComponent) {
+    'use strict';
+
+    return AppComponent.extend("riskmanagement.ricks.Component", {
+        metadata: {
+            manifest: "json"
+        }
+    });
+});
---
+++ app/services.cds
@@ -0,0 +1,4 @@
+
+using from './ricks/annotations';
+
+using from './common';
